41f6908007899b2ab8b90946a3658d8f3a2f86b9
keeganthomp <whereiskeegan@gmail.com>
2020-02-16 23:07:40 -0800

    cleaning up

--- app.js
+++ app.js
@@ -1,43 +1,19 @@
-// const express = require('express')
-// const router = express.Router()
-// const logger = require('morgan')
-// const { initializeRoutes } = require ('./routes')
-// const bodyParser = require('body-parser')
 const db = require('./models')
+require('dotenv').config()
 
+const server = require('./server')
 
-// const app = express()
-// app.use(logger('dev'))
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: false }))
-
-// initializeRoutes({ router })
-
-// initializeDatabaseAndApi = async () => {
-//   try {
-//     await db.sequelize.sync()
-//     app.listen(8000, () => console.log('App listening on port 8080!'))
-//   } catch (err) {
-//     console.log('Error conencting to database:', err)
-//   }
-// }
-
-// initializeDatabaseAndApi()
-
-require('dotenv').config();
-
-const server = require('./server');
-
-const PORT = process.env.PORT || 3300;
+const PORT = process.env.PORT || 3300
 
 initializeServer = async () => {
   try {
     await db.sequelize.sync()
-    server.listen(PORT, () => console.log(`Server is live at localhost:${PORT}`));
+    server.listen(PORT, () => {
+      console.log(`Server is live at localhost:${PORT}`)
+    })
   } catch (err) {
     console.log('Error conencting to database:', err)
   }
 }
 
 initializeServer()
-
